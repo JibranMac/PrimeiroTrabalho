e300dd2d3daead2532c04f6baea359668b951a22
JibranMac <137580845+JibranMac@users.noreply.github.com>
2023-09-12 23:52:05 -0300

    Primeira Fase

--- Historia/Historia/Historia.cpp
+++ Historia/Historia/Historia.cpp
@@ -1,60 +1,77 @@
+											
+											//Historia base para usar de exemplo.
+
+/*
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 int main() {
-    string nomePersonagem;
-    string escolha;
+	string nomePersonagem;
+	string escolha;
 
-    cout << "Bem-vindo a Aventura Magica!" << endl;
-    cout << "Digite o nome do personagem principal: " << endl;
-    cin >> nomePersonagem;
+	cout << "Bem-vindo a Aventura Magica!" << endl;
+	cout << "Digite o nome do personagem principal: " << endl;
+	cin >> nomePersonagem;
 
-    cout << "\n" << nomePersonagem << " e sua amiga Manuela estao em busca de uma espada lendaria para derrotar o terrivel Dragao de Fogo." << endl;
+	cout << "\n" << nomePersonagem << " e sua amiga Manuela estao em busca de uma espada lendaria para derrotar o terrivel Dragao de Fogo." << endl;
 
-    cout << "Voces encontram um bosque misterioso. Qual caminho voce escolhe? (A/B):\n";
-    cin >> escolha;
+	cout << "Voces encontram um bosque misterioso. Qual caminho voce escolhe? (A/B):\n";
+	cin >> escolha;
 
-    if (escolha == "A" || escolha == "a") {
-        cout << "\nVoces decidem entrar no bosque e encontram uma clareira com a espada lendaria! Agora, vocês devem seguir para o covil do Dragao de Fogo." << endl;
-    }
-    else if (escolha == "B" || escolha == "b") {
-        cout << "\nVoces decidem contornar o bosque e encontram um rio perigoso. O que voce faz? (A/B):\n";
-        cin >> escolha;
+	if (escolha == "A" || escolha == "a") {
+		cout << "\nVoces decidem entrar no bosque e encontram uma clareira com a espada lendaria! Agora, vocês devem seguir para o covil do Dragao de Fogo." << endl;
+	}
+	else if (escolha == "B" || escolha == "b") {
+		cout << "\nVoces decidem contornar o bosque e encontram um rio perigoso. O que voce faz? (A/B):\n";
+		cin >> escolha;
 
-        if (escolha == "A" || escolha == "a") {
-            cout << "\nVoces tentam atravessar o rio, mas sao arrastados pela correnteza e acabam perdendo tempo. Finalmente, voces encontram a espada lendaria e seguem para o covil do Dragao de Fogo." << endl;
-        }
-        else if (escolha == "B" || escolha == "b") {
-            cout << "\nVoces decidem seguir ao redor do rio e encontram um atalho para a espada lendaria! Agora, voces devem seguir para o covil do Dragao de Fogo." << endl;
-        }
-        else {
-            cout << "\nOpcao invalida. Você esta perdido na floresta." << endl;
-            return 0;
-        }
-    }
-    else {
-        cout << "\nOpcao invalida. Você esta perdido na floresta." << endl;
-        return 0;
-    }
+		if (escolha == "A" || escolha == "a") {
+			cout << "\nVoces tentam atravessar o rio, mas sao arrastados pela correnteza e acabam perdendo tempo. Finalmente, voces encontram a espada lendaria e seguem para o covil do Dragao de Fogo." << endl;
+		}
+		else if (escolha == "B" || escolha == "b") {
+			cout << "\nVoces decidem seguir ao redor do rio e encontram um atalho para a espada lendaria! Agora, voces devem seguir para o covil do Dragao de Fogo." << endl;
+		}
+		else {
+			cout << "\nOpcao invalida. Você esta perdido na floresta." << endl;
+			return 0;
+		}
+	}
+	else {
+		cout << "\nOpcao invalida. Você esta perdido na floresta." << endl;
+		return 0;
+	}
 
-    cout << "\nVoces finalmente chegam ao covil do Dragao de Fogo. O que voce faz? (A/B):\n";
-    cin >> escolha;
+	cout << "\nVoces finalmente chegam ao covil do Dragao de Fogo. O que voce faz? (A/B):\n";
+	cin >> escolha;
 
-    if (escolha == "A" || escolha == "a") {
-        cout << "\nVoces tentam falar com o Dragao de Fogo em uma tentativa de paz, mas ele ataca imediatamente! A batalha comeca!" << endl;
-    }
-    else if (escolha == "B" || escolha == "b") {
-        cout << "\nVoces entram no covil de forma furtiva e tentam roubar o tesouro sem acordar o Drageo de Fogo. Infelizmente, o Dragao acorda e a batalha comeca!" << endl;
-    }
-    else {
-        cout << "\nOpcao invalida. O Dragao de Fogo ataca enquanto voce esta indeciso!" << endl;
-    }
+	if (escolha == "A" || escolha == "a") {
+		cout << "\nVoces tentam falar com o Dragao de Fogo em uma tentativa de paz, mas ele ataca imediatamente! A batalha comeca!" << endl;
+	}
+	else if (escolha == "B" || escolha == "b") {
+		cout << "\nVoces entram no covil de forma furtiva e tentam roubar o tesouro sem acordar o Drageo de Fogo. Infelizmente, o Dragao acorda e a batalha comeca!" << endl;
+	}
+	else {
+		cout << "\nOpcao invalida. O Dragao de Fogo ataca enquanto voce esta indeciso!" << endl;
+	}
 
-    // Lógica da batalha com o Dragão de Fogo pode ser implementada aqui...
+	// Lógica da batalha com o Dragão de Fogo pode ser implementada aqui...
 
-    cout << "\nFim da aventura! O destino de " << nomePersonagem << " e Manuela sera revelado na proxima jornada." << endl;
+	cout << "\nFim da aventura! O destino de " << nomePersonagem << " e Manuela sera revelado na proxima jornada." << endl;
 
-    return 0;
+	return 0;
 }
+*/
+
+										//Inicio da Narrativa
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+}
+
