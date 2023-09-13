14263207f1254413bf409811277a23670cfbf9fb
JibranMac <137580845+JibranMac@users.noreply.github.com>
2023-09-12 23:20:04 -0300

    correção using namespace

--- Historia/Historia/Historia.cpp
+++ Historia/Historia/Historia.cpp
@@ -3,9 +3,11 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
 
 // Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
