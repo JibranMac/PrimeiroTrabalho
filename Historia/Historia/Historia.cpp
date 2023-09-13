996ca9f9422526c48f2afa699ffba1e466cc1bdc
JibranMac <137580845+JibranMac@users.noreply.github.com>
2023-09-12 23:43:50 -0300

    Historia Exemplo

--- Historia/Historia/Historia.cpp
+++ Historia/Historia/Historia.cpp
@@ -1,22 +1,60 @@
-// Historia.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!\n";
+int main() {
+    string nomePersonagem;
+    string escolha;
+
+    cout << "Bem-vindo a Aventura Magica!" << endl;
+    cout << "Digite o nome do personagem principal: " << endl;
+    cin >> nomePersonagem;
+
+    cout << "\n" << nomePersonagem << " e sua amiga Manuela estao em busca de uma espada lendaria para derrotar o terrivel Dragao de Fogo." << endl;
+
+    cout << "Voces encontram um bosque misterioso. Qual caminho voce escolhe? (A/B):\n";
+    cin >> escolha;
+
+    if (escolha == "A" || escolha == "a") {
+        cout << "\nVoces decidem entrar no bosque e encontram uma clareira com a espada lendaria! Agora, vocês devem seguir para o covil do Dragao de Fogo." << endl;
+    }
+    else if (escolha == "B" || escolha == "b") {
+        cout << "\nVoces decidem contornar o bosque e encontram um rio perigoso. O que voce faz? (A/B):\n";
+        cin >> escolha;
+
+        if (escolha == "A" || escolha == "a") {
+            cout << "\nVoces tentam atravessar o rio, mas sao arrastados pela correnteza e acabam perdendo tempo. Finalmente, voces encontram a espada lendaria e seguem para o covil do Dragao de Fogo." << endl;
+        }
+        else if (escolha == "B" || escolha == "b") {
+            cout << "\nVoces decidem seguir ao redor do rio e encontram um atalho para a espada lendaria! Agora, voces devem seguir para o covil do Dragao de Fogo." << endl;
+        }
+        else {
+            cout << "\nOpcao invalida. Você esta perdido na floresta." << endl;
+            return 0;
+        }
+    }
+    else {
+        cout << "\nOpcao invalida. Você esta perdido na floresta." << endl;
+        return 0;
+    }
+
+    cout << "\nVoces finalmente chegam ao covil do Dragao de Fogo. O que voce faz? (A/B):\n";
+    cin >> escolha;
+
+    if (escolha == "A" || escolha == "a") {
+        cout << "\nVoces tentam falar com o Dragao de Fogo em uma tentativa de paz, mas ele ataca imediatamente! A batalha comeca!" << endl;
+    }
+    else if (escolha == "B" || escolha == "b") {
+        cout << "\nVoces entram no covil de forma furtiva e tentam roubar o tesouro sem acordar o Drageo de Fogo. Infelizmente, o Dragao acorda e a batalha comeca!" << endl;
+    }
+    else {
+        cout << "\nOpcao invalida. O Dragao de Fogo ataca enquanto voce esta indeciso!" << endl;
+    }
+
+    // Lógica da batalha com o Dragão de Fogo pode ser implementada aqui...
+
+    cout << "\nFim da aventura! O destino de " << nomePersonagem << " e Manuela sera revelado na proxima jornada." << endl;
+
+    return 0;
 }
-
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
