3f87e11ca4b2f4b960ac7f1f26ec30d8e5659ece
JibranMac <137580845+JibranMac@users.noreply.github.com>
2023-09-13 23:42:04 -0300

    Mudança para adição de habilidades com diferença entre calsses e raças

--- Historia/Historia/Historia.cpp
+++ Historia/Historia/Historia.cpp
@@ -1,138 +1,145 @@
-											
-											//Historia base para usar de exemplo.
-
-/*
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 int main() {
-	string nomePersonagem;
-	string escolha;
+    // Variáveis do personagem
+    string escolha;
+    string nome;
+    string cabelo;
+    string raca;
+    string olhos;
+    string classe;
+    char genero;
+    int idade;
 
-	cout << "Bem-vindo a Aventura Magica!" << endl;
-	cout << "Digite o nome do personagem principal: " << endl;
-	cin >> nomePersonagem;
+    // Variáveis de Habilidades
+    int nivel = 0;
+    int ataque = 0;
+    int defesa = 0;
+    int agilidade = 0;
+    int magia = 0;
+    int vida = 0;
 
-	cout << "\n" << nomePersonagem << " e sua amiga Manuela estao em busca de uma espada lendaria para derrotar o terrivel Dragao de Fogo." << endl;
+    // Escolha de Personagem
+    cout << "    A Maldicao do Castelo!      " << endl;
+    cout << "\n";
+    cout << "Personalizacao de Personagem\n";
+    cout << "\n";
 
-	cout << "Voces encontram um bosque misterioso. Qual caminho voce escolhe? (A/B):\n";
-	cin >> escolha;
+    cout << "Digite o nome do seu personagem : \n";
+    cin.ignore();
+    getline(cin, nome);
 
-	if (escolha == "A" || escolha == "a") {
-		cout << "\nVoces decidem entrar no bosque e encontram uma clareira com a espada lendaria! Agora, vocês devem seguir para o covil do Dragao de Fogo." << endl;
-	}
-	else if (escolha == "B" || escolha == "b") {
-		cout << "\nVoces decidem contornar o bosque e encontram um rio perigoso. O que voce faz? (A/B):\n";
-		cin >> escolha;
+    cout << "Qual sua idade : \n";
+    cin >> idade;
 
-		if (escolha == "A" || escolha == "a") {
-			cout << "\nVoces tentam atravessar o rio, mas sao arrastados pela correnteza e acabam perdendo tempo. Finalmente, voces encontram a espada lendaria e seguem para o covil do Dragao de Fogo." << endl;
-		}
-		else if (escolha == "B" || escolha == "b") {
-			cout << "\nVoces decidem seguir ao redor do rio e encontram um atalho para a espada lendaria! Agora, voces devem seguir para o covil do Dragao de Fogo." << endl;
-		}
-		else {
-			cout << "\nOpcao invalida. Você esta perdido na floresta." << endl;
-			return 0;
-		}
-	}
-	else {
-		cout << "\nOpcao invalida. Você esta perdido na floresta." << endl;
-		return 0;
-	}
+    cout << "Qual seu Genero (M/F) : \n";
+    cin >> genero;
 
-	cout << "\nVoces finalmente chegam ao covil do Dragao de Fogo. O que voce faz? (A/B):\n";
-	cin >> escolha;
+    cout << "Qual a cor de seus olhos : \n";
+    cin >> olhos;
 
-	if (escolha == "A" || escolha == "a") {
-		cout << "\nVoces tentam falar com o Dragao de Fogo em uma tentativa de paz, mas ele ataca imediatamente! A batalha comeca!" << endl;
-	}
-	else if (escolha == "B" || escolha == "b") {
-		cout << "\nVoces entram no covil de forma furtiva e tentam roubar o tesouro sem acordar o Drageo de Fogo. Infelizmente, o Dragao acorda e a batalha comeca!" << endl;
-	}
-	else {
-		cout << "\nOpcao invalida. O Dragao de Fogo ataca enquanto voce esta indeciso!" << endl;
-	}
+    cout << "Qual a cor de seu cabelo : \n";
+    cin >> cabelo;
 
-	// Lógica da batalha com o Dragão de Fogo pode ser implementada aqui...
+    cout << "Qual a raca do seu personagem (Humano, Anao, Elfo, Orc) : \n";
+    cin >> raca;
 
-	cout << "\nFim da aventura! O destino de " << nomePersonagem << " e Manuela sera revelado na proxima jornada." << endl;
+    cout << "Qual seria sua classe (Mago, Arqueiro, Guerreiro, Escudeiro) : \n";
+    cin >> classe;
+    cout << "\n";
 
-	return 0;
-}
-*/
+    // Ajustando as habilidades com base na classe escolhida
+    if (classe == "Mago" || classe == "mago") {
+        nivel += 1;
+        agilidade += 5;
+        magia += 15;
+        defesa += 0;
+        ataque += 0;
+        vida += 0;
+    }
+    else if (classe == "Arqueiro" || classe == "arqueiro") {
+        nivel += 1;
+        agilidade += 5;
+        magia += 0;
+        defesa += 0;
+        ataque += 15;
+        vida += 0;
+    }
+    else if (classe == "Escudeiro" || classe == "escudeiro") {
+        nivel += 1;
+        agilidade += 0;
+        magia += 0;
+        defesa += 15;
+        ataque += 5;
+        vida += 0;
+    }
+    else if (classe == "Guerreiro" || classe == "guerreiro") {
+        nivel += 1;
+        agilidade += 0;
+        magia += 0;
+        defesa += 5;
+        ataque += 15;
+        vida += 0;
+    }
+    // Ajustando as habilidades com base na raça escolhida
+    if (raca == "Humano") {
+        agilidade += 5;
+        magia += 5;
+        defesa += 5;
+        ataque += 5;
+        vida += 10;
+    }
+    else if (raca == "Anao") {
+        agilidade += 1;
+        magia += 1;
+        defesa += 15;
+        ataque += 5;
+        vida += 20;
+    }
+    else if (raca == "Elfo") {
+        agilidade += 5;
+        magia += 10;
+        defesa += 1;
+        ataque += 5;
+        vida += 10;
+    }
+    else if (raca == "Orc") {
+        agilidade += 1;
+        magia += 1;
+        defesa += 15;
+        ataque += 5;
+        vida += 20;
+    }
 
-					//Inicio da Narrativa, A Maldicao do Castelo! (Capitulo 1)
+    // Continuação da história
+    cout << "A Maldicao do Castelo! (Capitulo 1)" << endl;
+    cout << "\n";
+    cout << "Habilidades" << endl;
+    cout << "\n";
+    cout << "Nivel : " << nivel << endl;
+    cout << "Ataque : " << ataque << endl;
+    cout << "Defesa : " << defesa << endl;
+    cout << "Vida : " << vida << endl;
+    cout << "Agilidade : " << agilidade << endl;
+    cout << "Magia : " << magia << endl;
+    cout << "\n";
 
-#include <iostream>
-#include <string>
+    // Início da história
+    cout << "Voce esta em uma cidade medieval, pertencente a uma guilda de aventureiros, buscando por uma nova missao emocionante." << endl;
+    cout << "De repente, um mensageiro real chega a guilda com uma proposta intrigante..." << endl;
+    cout << "Ele informa que um castelo misterioso nas proximidades esta assombrado por uma terrivel maldicao e a guilda esta em busca de um corajoso aventureiro para investigar e quebrar a maldicao." << endl;
+    cout << "Voce aceita esta missao? (S/N)" << endl;
+    cin >> escolha;
+    cout << "\n";
 
-using namespace std;
-
-int main() {
-					//Variaveis
-	string escolha;
-	string nome;
-	string cabelo;
-	string raca;
-	string olhos;
-	string classe;
-	char genero;
-	int idade;
-	
-					//Escolha de Personagem
-	cout << "    A Maldicao do Castelo!      " << endl;
-	cout << "\n";
-	cout << "Personalizacao de Personagem\n";
-	cout << "\n";
-
-	cout << "Digite o nome do seu personagem : \n";
-	getline(cin, nome);
-	
-	cout << "Qual sua idade : \n";
-	cin >> idade;
-
-	cout << "Qual seu Genero (F/M) : \n";
-	cin >> genero;
-
-	cout << "Qual a cor de seus olhos : \n";
-	cin >> olhos;
-
-	cout << "Qual a cor de seu cabelo : \n";
-	cin >> cabelo;
-
-	cout << "Qual a raca do seu personagem(Humano, Anao, Elfo, Orc) : \n";
-	cin >> raca;
-
-	cout << "Qual seria sua classe (Mago, Arqueiro, Guerreiro, Escudeiro) : \n";
-	cin >> classe;
-	cout << "\n";
-					//Fim da Escolha de Personagem
-	cout << "Parabens, voce escolheu as caracteristicas do seu personagem " << nome << " , agora vamos comecar nossa aventura!!\n";
-	cout << "\n";
-	cout << "\n";
-				
-	cout << "A Maldicao do Castelo! (Capitulo 1)";
-	cout << "\n";
-	cout << "Habilidades\n";
-	cout << "\n";
-	cout << "Nivel : 1\n";
-	cout << "Ataque : 1\n";
-	cout << "Defesa : 1\n";
-	cout << "Agilidade : 1\n";
-	cout << "Magia : 1\n";
-	cout << "\n";
-					//Inicio Da Historia
-	cout << "Vamos comecar?(S/N)\n";
-	cin >> escolha;
-
-	if (escolha == "S" || escolha == "s" || escolha == "Sim" || escolha == "SIM" || escolha == "sim") {
-		cout << "Era uma vez " << nome << " vagando pelas florestas obscuras do mundo!\n";
-	}
-	else if (escolha == "N" || escolha == "n" || escolha == "Nao" || escolha == "NAO" || escolha == "nao") {
-		cout << "Entao vai se... kkkkkk coringuei\n";
-	}
-	return 0;
+    if (escolha == "S" || escolha == "s" || escolha == "Sim" || escolha == "SIM" || escolha == "sim") {
+        cout << "Decidindo corajosamente aceitar a missao, voce parte em direcao ao castelo assombrado, preparado para enfrentar os desafios que aguardam." << endl;
+    }
+    else if (escolha == "N" || escolha == "n" || escolha == "Não" || escolha == "NÃO" || escolha == "não") {
+        cout << "Optando por nao aceitar a missao, voce decide continuar sua jornada em busca de outras aventuras e misterios." << endl;
+    }
+    return 0;
 }
